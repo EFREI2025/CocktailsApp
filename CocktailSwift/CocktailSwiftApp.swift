369e499f8ac9122e5ab4ec5b69386616f3ec8e81
georgyguei <georgy.guei@efrei.net>
2025-12-17 14:55:35 +0100

    chore: add initial project files, including .gitignore, Xcode project settings, and basic SwiftUI app structure

---
+++ CocktailSwift/CocktailSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailSwiftApp.swift
+//  CocktailSwift
+//
+//  Created by Georgy GUEI on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CocktailSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
